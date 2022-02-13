8fba27c9a618f17797437ec3327eae7ce94ab7a7
collins <collinsboniface1000@gmail.com>
2022-02-14 01:51:37 +0200

    Created a new file to give more clarification about the argParseCLI python file by creating a map of sizes and crusts with a helper function

---
+++ argParse/argParseCLI.py
@@ -0,0 +1,25 @@
+import argparse
+
+SIZES = {
+    "s": "Small"
+    "m": "Medium"
+    "l": "Large"
+    "x1": "Extra Large"
+    "xx1": "Extra extra large"
+}
+
+CRUSTS = {"normal": "", "thin": " thin crust", "deep": "deep dish"}
+
+def build_pizza(order):
+    pizza = f"{SIZES[order['size']]}{CRUSTS[order['crust']]}"
+    if order.get("toppings"):
+        pizza += " With " + ", ".join(order["toppings"])
+    if order.get("extra-cheese"):
+        pizza += " plus extra cheese"
+    if order.get("extra-sauce"):
+        pizza += " and extra sauce"
+    return pizza
+
+
+if __name__ == "__main__":
+
