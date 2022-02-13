661a51cd84e76ab0b793b8bc9294d881e0350b7e
collins <collinsboniface1000@gmail.com>
2022-02-14 01:14:34 +0200

    Created the main function and used the if condition

--- Socket/server.py
+++ Socket/server.py
@@ -4,7 +4,16 @@ import socket
 # It's going to give you the virtual box ip address
 # host = socket.gethostbyname(socket.gethostname())
 
-HOST = ''
-PORT = 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    HOST = 'localhost'
+    PORT = '8080'
+    result = server.connect_ex((HOST, PORT))
+    print("Result is {}".format(result))
+    server.close()
+
+if __name__ = "__main__":
+    main()
