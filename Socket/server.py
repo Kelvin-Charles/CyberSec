9834e70c22a1013149ae3d19a72f14b8fa4c8a2b
collins <collinsboniface1000@gmail.com>
2022-02-14 01:18:41 +0200

    Removed the Single quotes from the port number and fixed the assignment operator to comparison operator

--- Socket/server.py
+++ Socket/server.py
@@ -5,15 +5,14 @@ import socket
 # host = socket.gethostbyname(socket.gethostname())
 
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     HOST = 'localhost'
-    PORT = '8080'
+    PORT = 8080
     result = server.connect_ex((HOST, PORT))
     print("Result is {}".format(result))
     server.close()
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
