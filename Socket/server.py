b3aead7f31e045effbf8a5976e33ea45f35d74c2
collins <collinsboniface1000@gmail.com>
2022-02-13 23:58:51 +0200

    Added the comment to the output of using the gethostbyname method

--- Socket/server.py
+++ Socket/server.py
@@ -1,7 +1,9 @@
 import socket
 
 # Private ip Address
+# It's going to give you the virtual box ip address
 # host = socket.gethostbyname(socket.gethostname())
+
 HOST = ''
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
