b735746a7909b125ecf2b7c1946e3987b9fe23a9
collins <collinsboniface1000@gmail.com>
2022-02-14 07:29:10 +0200

    Fixed the Syntax error for the TCP and the timefunc

--- Socket/Socket.py
+++ Socket/Socket.py
@@ -19,7 +19,7 @@ class Scanner:
                 self.add_port(port)
 
     def is_open(self, port):
-        s = socket.socket(socket.AF_INET, socket.SOCKE_STREAM)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = s.connect_ex((self.ip, port))
         s.close()
         return result == 0
@@ -29,7 +29,7 @@ class Scanner:
         with open(filepath, 'w') as f:
             f.write('\n'.join(open_ports))
 
-@timuefunc
+@timefunc
 def main():
     # IP to scan for open ports
     ip = '172.20.10.9'
@@ -39,5 +39,5 @@ def main():
     print(scanner.open_ports)
 
 
-if __main__ = "__main__":
+if __name__ == "__main__":
     main()
