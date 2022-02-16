cc1ca23f5e2d42327d54cf8e128398fe443e7243
collins <collinsboniface1000@gmail.com>
2022-02-16 04:53:43 +0200

    Used the decorator to get the output of the choice number and find the square of the number

--- Decorators/decorator3.py
+++ Decorators/decorator3.py
@@ -2,6 +2,19 @@ import random
 
 # Decorators with arguments
 
+def power_of_2(func):
+
+    def inner():
+
+        return func() ** 2
+
+    return inner
+
+@power_of_2
 def random_odd_digits():
 
-    return random.choices([1, 3, 5, 7, 9])
+    return random.choice([1, 3, 5, 7, 9])
+
+print(random_odd_digits())
+
+
